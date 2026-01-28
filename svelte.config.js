6162ba39684fed4b80fd66c2f7575a3efdc9e67f
rmccownISoft <60158596+rmccownISoft@users.noreply.github.com>
2026-01-27 20:30:35 -0600

    chore: enable inspector

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,13 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-node'
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+	vitePlugin: {
+		inspector: true
+	},
+	kit: {
+		adapter: adapter()
+	}
+}
 
-export default config;
+export default config
